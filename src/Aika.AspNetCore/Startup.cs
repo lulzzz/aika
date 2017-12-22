b239930c69205a5af11f36cb067e42a32f478f64
wazzamatazz <grahamcharleswatts@gmail.com>
2017-12-23 00:03:00 +0200

    Added task runner
    Added TaskRunner service.
    Added default authenticated user to every request in ASP.NET Core app.

--- src/Aika.AspNetCore/Startup.cs
+++ src/Aika.AspNetCore/Startup.cs
@@ -2,38 +2,54 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
-namespace Aika.AspNetCore
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace Aika.AspNetCore {
+    public class Startup {
+        public Startup(IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddLogging(x => x.AddConsole());
+            services.AddAuthentication();
             services.AddMvc();
+            services.AddTaskRunnerService();
+            services.AddSingleton<IHistorian, Aika.Historians.InMemoryHistorian>();
             services.AddSingleton<AikaHistorian>();
         }
 
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) {
+            if (env.IsDevelopment()) {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Middleware that adds all callers to all Aika roles.
+            app.Use((context, next) => {
+                var identity = new System.Security.Claims.ClaimsIdentity("fixed");
+                identity.AddClaim(new System.Security.Claims.Claim(identity.NameClaimType, "TestUser"));
+                identity.AddClaim(new System.Security.Claims.Claim(identity.RoleClaimType, Roles.Administrator));
+                identity.AddClaim(new System.Security.Claims.Claim(identity.RoleClaimType, Roles.ManageTags));
+                identity.AddClaim(new System.Security.Claims.Claim(identity.RoleClaimType, Roles.ReadTagData));
+                identity.AddClaim(new System.Security.Claims.Claim(identity.RoleClaimType, Roles.WriteTagData));
+
+                context.User = new System.Security.Claims.ClaimsPrincipal(identity);
+
+                return next();
+            });
 
             app.UseMvc();
         }
---
+++ src/Aika.AspNetCore/TaskRunner.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Aika.AspNetCore
+{
+    public class TaskRunner : IHostedService, ITaskRunner, IDisposable {
+
+        private readonly ILogger _log;
+
+        private readonly CancellationTokenSource _cancellationTokenSource = new CancellationTokenSource();
+
+        private readonly ConcurrentDictionary<Task, object> _tasks = new ConcurrentDictionary<Task, object>();
+
+
+        public TaskRunner(ILoggerFactory loggerFactory) {
+            _log = loggerFactory?.CreateLogger<TaskRunner>();
+        }
+
+
+        public Task StartAsync(CancellationToken cancellationToken) {
+            _log?.LogInformation("Startup requested.");
+            return Task.Delay(-1, cancellationToken);
+        }
+
+
+        public async Task StopAsync(CancellationToken cancellationToken) {
+            if (_cancellationTokenSource.IsCancellationRequested) {
+                return;
+            }
+
+            _log?.LogInformation("Shutdown requested.");
+            Task[] tasks;
+
+            try {
+                _cancellationTokenSource.Cancel();
+            }
+            finally {
+                tasks = _tasks.Keys.ToArray();
+            }
+
+            var remainingTasks = Task.WhenAll(tasks);
+            await Task.WhenAny(remainingTasks, Task.Delay(-1, cancellationToken)).ConfigureAwait(false);
+
+        }
+
+
+        public void RunBackgroundTask(Func<CancellationToken, Task> action, params CancellationToken[] cancellationTokens) {
+            if (_cancellationTokenSource.IsCancellationRequested) {
+                return;
+            }
+
+            var task = Task.Run(async () => {
+                try {
+                    using (var ct = CancellationTokenSource.CreateLinkedTokenSource(cancellationTokens.Concat(new[] { _cancellationTokenSource.Token }).ToArray())) {
+                        await action(ct.Token).ConfigureAwait(false);
+                    }
+                }
+                catch (OperationCanceledException) {
+                    // Do nothing.
+                }
+                catch (Exception e) {
+                    _log?.LogError($"An unhandled exception occurred in a background task.", e);
+                }
+            }, _cancellationTokenSource.Token);
+
+            _tasks[task] = new object();
+
+            task.ContinueWith(t => {
+                _tasks.TryRemove(t, out var _);
+            });
+        }
+
+
+        public void Dispose() {
+            _cancellationTokenSource.Dispose();
+        }
+
+    }
+}
